963b29ac79c9710f7cfd7f11a8b30f47c9258356
deijaparker <deijaparker1@outlook.com>
2024-08-05 21:42:29 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+// Part 1: Getting Started
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+// Part 2: Creating a Menu Bar
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Part 3: Adding Menu Buttons
+const menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+menuLinks.forEach((link) => {
+  const a = document.createElement("a");
+  a.setAttribute("href", link.href);
+  a.textContent = link.text;
+  topMenuEl.appendChild(a);
+});
+
+// Part 4: Adding Interactivity (to be continued in Part Two)
